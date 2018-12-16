4870b41092a858d5326c36a0515a7b35471a5939
sebastianchoo <sebastian_choo>
2018-12-16 21:38:41 +0800

    announcement Screen 1/2 Done

--- src/AnnouncementScreen.js
+++ src/AnnouncementScreen.js
@@ -9,6 +9,7 @@ import Svg, {
   Stop
 } from "react-native-svg";
 import {Fonts} from "./Constants";
+import {TabView, TabBar, SceneMap} from 'react-native-tab-view';
 
 class AnnouncementScreen extends React.Component {
   constructor(props) {
@@ -48,53 +49,53 @@ class AnnouncementScreen extends React.Component {
     })
   }
 
-  bottomCurveColor() {
-    switch (this.props.userDetailsReducer.house) {
-      case 'Red':
-        return '#af4448'
-      case 'Black':
-        return '#373737'
-      case 'Green':
-        return "#519657"
-      case 'Yellow':
-        return "#fdd835"
-      case 'Blue':
-        return "#0093c4"
-    }
-  }
+  // bottomCurveColor() {
+  //   switch (this.props.userDetailsReducer.house) {
+  //     case 'Red':
+  //       return '#af4448'
+  //     case 'Black':
+  //       return '#373737'
+  //     case 'Green':
+  //       return "#519657"
+  //     case 'Yellow':
+  //       return "#fdd835"
+  //     case 'Blue':
+  //       return "#0093c4"
+  //   }
+  // }
 
   render() {
     return (
-        <View style={{
-          backgroundColor: 'white',
-          flex: 1
+      <View style={{
+        backgroundColor: 'white',
+        flex: 1
+      }}>
+        <Svg style={{
+          position: 'absolute',
+          height: this.state.height,
+          width: this.state.width
         }}>
-          <Svg style={{
-            position: 'absolute',
-            height: this.state.height,
-            width: this.state.width
-          }}>
-            <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="blue">
-              <Stop stopColor="#4fc3f7" stopOpacity="1" offset="0%"/>
-              <Stop stopColor="#0093c4" stopOpacity="1" offset="100%"/>
-            </LinearGradient>
-            <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="red">
-              <Stop stopColor="#e57373" stopOpacity="1" offset="0%"/>
-              <Stop stopColor="#af4448" stopOpacity="1" offset="100%"/>
-            </LinearGradient>
-            <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="green">
-              <Stop stopColor="#81c784" stopOpacity="1" offset="0%"/>
-              <Stop stopColor="#519657" stopOpacity="1" offset="100%"/>
-            </LinearGradient>
-            <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="yellow">
-              <Stop stopColor="#fff176" stopOpacity="1" offset="0%"/>
-              <Stop stopColor="#fdd835" stopOpacity="1" offset="100%"/>
-            </LinearGradient>
-            <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="black">
-              <Stop stopColor="#616161" stopOpacity="1" offset="0%"/>
-              <Stop stopColor="#373737" stopOpacity="1" offset="100%"/>
-            </LinearGradient>
-            <Path d={`
+          <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="blue">
+            <Stop stopColor="#4fc3f7" stopOpacity="1" offset="0%"/>
+            <Stop stopColor="#0093c4" stopOpacity="1" offset="100%"/>
+          </LinearGradient>
+          <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="red">
+            <Stop stopColor="#e57373" stopOpacity="1" offset="0%"/>
+            <Stop stopColor="#af4448" stopOpacity="1" offset="100%"/>
+          </LinearGradient>
+          <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="green">
+            <Stop stopColor="#81c784" stopOpacity="1" offset="0%"/>
+            <Stop stopColor="#519657" stopOpacity="1" offset="100%"/>
+          </LinearGradient>
+          <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="yellow">
+            <Stop stopColor="#fff176" stopOpacity="1" offset="0%"/>
+            <Stop stopColor="#fdd835" stopOpacity="1" offset="100%"/>
+          </LinearGradient>
+          <LinearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="black">
+            <Stop stopColor="#616161" stopOpacity="1" offset="0%"/>
+            <Stop stopColor="#373737" stopOpacity="1" offset="100%"/>
+          </LinearGradient>
+          <Path d={`
               M0 0
               L${this.state.width} 0
               L0 ${this.state.svgHeight / 3}
@@ -102,34 +103,67 @@ class AnnouncementScreen extends React.Component {
               L${this.state.width} 0
               L0 ${this.state.svgHeight / 3}
             `} fill={`url(#${this.props.userDetailsReducer.house.toLowerCase()})`}/>
-            <Path d={`
-              M-5 ${this.state.svgHeight / 3 + 1}
-              C${this.state.width / 3} ${this.state.svgHeight / 4} ${this.state.width / 2} ${this.state.svgHeight / 2.5} ${this.state.width} ${this.state.svgHeight / 3 + 1}
-            `} fill={'white'}/>
-            <Path d={`
-              M${this.state.width + 5} ${this.state.svgHeight / 3 - 1}
-              S${this.state.width / 100 * 80} ${this.state.svgHeight / 2.5} ${this.state.width / 2.2} ${this.state.svgHeight / 3 - 1}
-            `} fill={this.bottomCurveColor()}/>
-          </Svg>
-          <SafeAreaView style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-            marginTop: this.state.height / 17
+          {/*<Path d={`*/}
+          {/*M-5 ${this.state.svgHeight / 3 + 1}*/}
+          {/*C${this.state.width / 3} ${this.state.svgHeight / 4} ${this.state.width / 2} ${this.state.svgHeight / 2.5} ${this.state.width} ${this.state.svgHeight / 3 + 1}*/}
+          {/*`} fill={'white'}/>*/}
+          {/*<Path d={`*/}
+          {/*M${this.state.width + 5} ${this.state.svgHeight / 3 - 1}*/}
+          {/*S${this.state.width / 100 * 80} ${this.state.svgHeight / 2.5} ${this.state.width / 2.2} ${this.state.svgHeight / 3 - 1}*/}
+          {/*`} fill={this.bottomCurveColor()}/>*/}
+
+        </Svg>
+        <SafeAreaView style={{
+          height: this.state.svgHeight / 3,
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+          <Text style={{
+            fontFamily: Fonts.MEDIUM,
+            fontSize: 20,
+            color: 'white'
           }}>
-            <Text style={{
-              fontFamily: Fonts.MEDIUM,
-              fontSize: 20,
-              color: 'white'
-            }}>
-              Announcements
-            </Text>
-          </SafeAreaView>
-          <View style={{
-            flex: 12
-          }}>
-          </View>
+            Announcements
+          </Text>
+        </SafeAreaView>
+        <View style={{
+          flex: 12,
+        }}>
+          <TabBar
+            initialLayout={{
+              width: this.state.width
+            }}
+            navigationState={{
+              index: 0,
+              routes: [{
+                key: 'House',
+                title: 'House',
+              },
+                {
+                  key: 'All',
+                  title: 'All',
+                }],
+            }}
+            renderScene={SceneMap({
+              'House': () => {
+                return (
+                  <View style={{
+                    flex: 1,
+                  }}/>
+                )
+              },
+              'All': () => {
+                return (
+                  <View style={{
+                    flex: 1,
+                  }}/>
+                )
+              }
+            })}
+          />
         </View>
+      </View>
     );
   }
 }
